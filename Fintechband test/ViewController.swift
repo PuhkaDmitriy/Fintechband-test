5f1a77126b6f7c081b662abced3c0cbc13574e33
PuhkaDmitriy <dmitriy.puchka@xloo.com>
2019-07-08 11:50:02 +0300

    Initial Commit

---
+++ Fintechband test/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Fintechband test
+//
+//  Created by Дмитрий Пучка on 7/8/19.
+//  Copyright © 2019 Дмитрий Пучка. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
